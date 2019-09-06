9758018688b52978c75cec8a778335c727131c1a
samirdangal <samirdangal@gmail.com>
2019-09-06 11:33:29 +0545

    moor integration, create database, crud operation

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'data/moor_database.dart';
+import 'home_page.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Provider(
+      // The single instance of AppDatabase
+      builder: (_) => AppDatabase(),
+      child: MaterialApp(
+        title: 'Material App',
+        home: HomePage(),
+      ),
+    );
+  }
+}
