3a5df71210c36fb61efa9ed22971e8cc8cded002
samirdangal <samirdangal@gmail.com>
2019-09-09 14:01:35 +0545

    add task with tag

--- lib/main.dart
+++ lib/main.dart
@@ -2,16 +2,19 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import 'data/moor_database.dart';
-import 'home_page.dart';
+import 'ui/home_page.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Provider(
-      // The single instance of AppDatabase
-      builder: (_) => AppDatabase().taskDao,
+    final db = AppDatabase();
+    return MultiProvider(
+      providers: [
+        Provider(builder: (_) => db.taskDao),
+        Provider(builder: (_) => db.tagDao),
+      ],
       child: MaterialApp(
         title: 'Material App',
         home: HomePage(),
--- lib/ui/widget/new_task_input_widget.dart
+++ lib/ui/widget/new_task_input_widget.dart
@@ -2,8 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:moor_flutter/moor_flutter.dart';
 import 'package:provider/provider.dart';
 
-import 'data/moor_database.dart';
-
+import '../../data/moor_database.dart';
 
 class NewTaskInput extends StatefulWidget {
   const NewTaskInput({
@@ -16,6 +15,7 @@ class NewTaskInput extends StatefulWidget {
 
 class _NewTaskInputState extends State<NewTaskInput> {
   DateTime newTaskDate;
+  Tag selectedTag;
   TextEditingController controller;
 
   @override
@@ -29,9 +29,10 @@ class _NewTaskInputState extends State<NewTaskInput> {
     return Container(
       padding: const EdgeInsets.all(8.0),
       child: Row(
-        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: <Widget>[
           _buildTextField(context),
+          _buildTagSelector(context),
           _buildDateButton(context),
         ],
       ),
@@ -40,6 +41,7 @@ class _NewTaskInputState extends State<NewTaskInput> {
 
   Expanded _buildTextField(BuildContext context) {
     return Expanded(
+      flex: 1,
       child: TextField(
         controller: controller,
         decoration: InputDecoration(hintText: 'Task Name'),
@@ -48,11 +50,65 @@ class _NewTaskInputState extends State<NewTaskInput> {
           final task = TasksCompanion(
             name: Value(inputName),
             dueDate: Value(newTaskDate),
+            tagName: Value(selectedTag?.name),
           );
           dao.insertTask(task);
           resetValuesAfterSubmit();
         },
       ),
+    );
+  }
+
+  StreamBuilder<List<Tag>> _buildTagSelector(BuildContext context) {
+    return StreamBuilder<List<Tag>>(
+      stream: Provider.of<TagDao>(context).watchTags(),
+      builder: (context, snapshot) {
+        final tags = snapshot.data ?? List();
+
+        DropdownMenuItem<Tag> dropdownFromTag(Tag tag) {
+          return DropdownMenuItem(
+            value: tag,
+            child: Row(
+              children: <Widget>[
+                Text(tag.name),
+                SizedBox(width: 5),
+                Container(
+                  width: 15,
+                  height: 15,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Color(tag.color),
+                  ),
+                ),
+              ],
+            ),
+          );
+        }
+
+        final dropdownMenuItems =
+            tags.map((tag) => dropdownFromTag(tag)).toList()
+              // Add a "no tag" item as the first element of the list
+              ..insert(
+                0,
+                DropdownMenuItem(
+                  value: null,
+                  child: Text('No Tag'),
+                ),
+              );
+
+        return Expanded(
+          child: DropdownButton(
+            onChanged: (Tag tag) {
+              setState(() {
+                selectedTag = tag;
+              });
+            },
+            isExpanded: true,
+            value: selectedTag,
+            items: dropdownMenuItems,
+          ),
+        );
+      },
     );
   }
 
@@ -73,6 +129,7 @@ class _NewTaskInputState extends State<NewTaskInput> {
   void resetValuesAfterSubmit() {
     setState(() {
       newTaskDate = null;
+      selectedTag = null;
       controller.clear();
     });
   }
